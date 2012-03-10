adb6f15bdfd8858de1ba33f3a3b709d96fd654a0
Evgeniy Franskevich <johnyxxxxxx@gmail.com>
2012-03-10 18:04:47 +0700

    первый коммит

---
+++ main.js
@@ -0,0 +1,213 @@
+function drawBalance(xml)
+{		
+	var rub;
+	//try
+	{		
+		var balance = $("balance", xml);
+		if (balance.length)
+		{
+			rub = parseFloat(balance[0].text);
+			
+			$('#balance').text(balance[0].text+' р.');
+			$('#balance_docked').text(balance[0].text+' р.');
+		}
+		
+		var debetBound = $("debetBound", xml);
+		if (debetBound.length)
+		{
+			$('#debetBound').text(debetBound[0].text+ ' р.');
+		}
+
+		var days2BlockStr = $("days2BlockStr", xml);
+		if (days2BlockStr.length)
+		{
+			$('#days2BlockStr').text(days2BlockStr[0].text);
+			$('#days2BlockStr_docked').text(days2BlockStr[0].text);
+		}
+		
+		
+		var contractId = $("contractId", xml);
+		if (contractId.length)
+		{
+			$('#contractId').text(contractId[0].text);
+		}
+		
+		/*var promisedPayment = $("promisedPayment", xml);
+		if (promisedPayment.length)
+		{
+			var isOpen = promisedPayment.find("isOpen");
+			if (isOpen.length && isOpen[0].text == "1")
+				$('#debet').get(0).style.display = '';
+			else
+				$('#debet').get(0).style.display = 'none';
+		}*/
+	
+		var name = $("name", xml);
+		if (name.length)
+		{			
+			var reg = /(\D+)\s(\D+)\s(\D+)/;
+			var arr = reg.exec(name[0].text);
+			
+			var text = $('#first_name')[0].childNodes;
+			text[0].nodeValue = arr[1];
+			text[2].nodeValue = arr[2];
+			text[4].nodeValue = arr[3];
+		}	
+	}
+	//catch (err)
+	{
+		/*$('#gadgetContent').get(0).style.display = 'none';
+		$('#message').get(0).style.display = '';
+		$('#message').text(err.message);
+		return false;*/
+	}
+
+	if (rub)
+	{
+		var date = new Date();
+		var strDate = date.getDate() + "."+ (date.getMonth()+1) + "." + date.getFullYear() + 
+			" " + date.getHours() + ":" + date.getMinutes();
+			
+		$('#date').text(strDate);
+	
+		return true;
+	}
+	
+	return false;
+}
+
+function draw()
+{	
+	var xmlhttp = new XMLHttpRequest();
+
+	var login = xor(System.Gadget.Settings.readString('login'));
+	var password = xor(System.Gadget.Settings.readString('password'));
+
+	try 
+	{
+		//xmlhttp.open('POST', 'https://billing.novotelecom.ru/billing/user/index?act=login', true);
+		//xmlhttp.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+		//xmlhttp.setRequestHeader("Connection", "close");
+		//xmlhttp.send('backurl=&login=' + login +'&private_password='+password+'&+Enter+=%C2%EE%E9%F2%E8');
+
+		xmlhttp.open('POST', 'https://billing.novotelecom.ru/billing/user/api/?method=userInfo&login='+login+'&password=' + password, true);
+		xmlhttp.setRequestHeader('Content-Type', 'text/xml; charset=utf-8');
+		xmlhttp.setRequestHeader("Connection", "close");		
+		xmlhttp.send(null);
+		
+		xmlhttp.onreadystatechange = function() 
+		{
+			if (xmlhttp.readyState == 4 && xmlhttp.status == 200) 
+				try
+				{
+					if (!drawBalance(xmlhttp.responseXML)){
+						$('#gadgetContent').get(0).style.display = 'none';
+						$('#message').get(0).style.display = '';
+						$('#message').text(strings.connectError);								
+					}
+					else{		
+						$('#gadgetContent').get(0).style.display = '';
+						$('#message').get(0).style.display = 'none';
+					}
+				}
+				catch (err)
+				{
+					$('#gadgetContent').get(0).style.display = 'none';
+					$('#message').get(0).style.display = '';
+					$('#message').text(err.message);
+				}
+		}
+	}
+	catch (exception_var) 
+	{
+	}	
+}
+
+function onSettingsClosed(event) {
+    if (event.closeAction == event.Action.commit) {
+		initSettings();	
+    }
+}
+
+function initSettings() {
+    System.Gadget.settingsUI = 'settings.html';
+    System.Gadget.onSettingsClosed = onSettingsClosed;	
+	
+	//System.Gadget.Settings.write("PrivateSetting_GadgetOpacity", 40);
+	
+	// устанавливаем значения по умолчанию при первом запуске гаджета
+	var login = System.Gadget.Settings.readString('login');
+	var password = System.Gadget.Settings.readString('password');
+	if(login == '' || password == '') {
+		$('#gadgetContent').get(0).style.display = 'none';
+		$('#message').text(strings.inputLogin);
+		$('#message').get(0).style.display = 'inline-block';
+	}
+	else{
+		$('#gadgetContent').get(0).style.display = 'none';
+		$('#message').text(strings.connecting);
+		$('#message').get(0).style.display = '';
+		draw();
+	}
+}
+	
+function main()
+{
+    System.Gadget.onDock = dockStateChanged;
+    System.Gadget.onUndock = dockStateChanged;
+	
+	initSettings();
+	
+	/*{
+		// Для тестов
+	
+		$('#balance').text(194.41+' р.');
+		$('#balance_docked').text(194.41+' р.');
+
+		$('#debetBound').text(-300+ ' р.');	
+		
+		$('#days2BlockStr').text("Хватит примерно на 9 дней");
+				
+		$('#gadgetContent').get(0).style.display = '';
+		$('#message').get(0).style.display = 'none';
+	}*/
+	
+	window.setInterval(draw , 1000*60*60) 
+}
+	
+function dockStateChanged() {
+
+    if (System.Gadget.docked) {
+		var width = '100px';
+		var height = '85px';
+	
+		$('#background').css('width', width)
+						.css('height', height)
+						.attr('src', 'images/dockedBg.png');
+	
+        $(document.body).css('width', width)
+						.css('height', height);
+						
+		 $('#docked').get(0).style.display = '';
+		 $('#undocked').get(0).style.display = 'none';
+						
+	} else {
+		var width = '220px'
+		var height = '176px'
+		
+		$('#background').css('width', width)
+						.css('height', height)
+						.attr('src', 'images/dockedBg.png');
+
+        $(document.body).css('width', width)
+						.css('height', height);
+						
+		$('#docked').get(0).style.display = 'none';
+		$('#undocked').get(0).style.display = '';
+    }
+	
+	// Добавляем тень
+	//$('#background').get(0).addShadow("black", 20, 80, 5, 5);
+}
+
+$(document).ready(main);
---
+++ settings.js
@@ -0,0 +1,37 @@
+﻿function settingsClosing(event) {
+    if (event.closeAction == event.Action.commit) {
+		
+		// убираем выделение ошибок
+		$('.error').removeClass('error');
+		
+		if(!$('#login').val()) {
+			event.cancel = true;
+					
+			// показываем ошибку
+			$('#login').addClass('error');
+		}
+		
+		if (!$('#password').val())
+		{
+			event.cancel = true;
+			
+			// показываем ошибку
+			$('#password').addClass('error');			
+		}
+		
+		if(!event.cancel) {	
+			System.Gadget.Settings.writeString('login', xor($('#login').val()));
+			System.Gadget.Settings.writeString('password', xor($('#password').val()));
+		}		
+    }
+}
+
+function main() {
+    System.Gadget.onSettingsClosing = settingsClosing;
+	
+	// считываем старые значения настроек и показываем их в форме
+	$('#login').val(xor(System.Gadget.Settings.readString('login')));
+	$('#password').val(xor(System.Gadget.Settings.readString('password')));
+}
+
+$(document).ready(main);
